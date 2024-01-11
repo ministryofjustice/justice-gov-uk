0e1b00b1abd913af73dd54955699018d7f6df8a6
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2024-01-11 16:44:55 +0000

    Improve info.php

--- public/info.php
+++ public/info.php
@@ -3,10 +3,10 @@
 # Fill our vars and run on cli
 # $ php -f db-connect-test.php
 
-$dbname = $_ENV["DB_NAME"];
-$dbuser = $_ENV["DB_USER"];
-$dbpass = $_ENV["DB_PASSWORD"];
-$dbhost = $_ENV["DB_HOST"];
+$dbname = getenv("DB_NAME");
+$dbuser = getenv("DB_USER");
+$dbpass = getenv("DB_PASSWORD");
+$dbhost = getenv("DB_HOST");
 
 $connect = mysqli_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost'");
 mysqli_select_db($connect, $dbname) or die("Could not open the db '$dbname'");
@@ -17,7 +17,7 @@ $result = mysqli_query($connect, $test_query);
 $tblCnt = 0;
 while ($tbl = mysqli_fetch_array($result)) {
     $tblCnt++;
-    #echo $tbl[0]."<br />\n";
+    echo $tbl[0]."<br />\n";
 }
 
 if (!$tblCnt) {
