cc31dd388a75ba8c851b0da4f93ac4a961e5d5dc
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2024-01-19 15:23:29 +0000

    Only show info on development

--- public/info.php
+++ public/info.php
@@ -1,29 +1,41 @@
 <?php
 
-# Fill our vars and run on cli
-# $ php -f db-connect-test.php
+if (getenv('WP_ENV') !== 'development') {
+    header($_SERVER["SERVER_PROTOCOL"] . " 404 Not Found");
+    exit;
+}
+
+## -------------------------------------------------------------------------
+## -------------------------------------------------------------------------
 
 $dbname = getenv("DB_NAME");
-$dbuser = getenv("DB_USER");
-$dbpass = getenv("DB_PASSWORD");
-$dbhost = getenv("DB_HOST");
 
-$connect = mysqli_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost'");
+# connect to the DB
+$connect = mysqli_connect(
+    getenv("DB_HOST"),
+    getenv("DB_USER"),
+    getenv("DB_PASSWORD")
+) or die("Unable to Connect to " . getenv("DB_HOST"));
+
+# select the DB by name
 mysqli_select_db($connect, $dbname) or die("Could not open the db '$dbname'");
 
-$test_query = "SHOW TABLES FROM $dbname";
-$result = mysqli_query($connect, $test_query);
+# query the tables
+$result = mysqli_query($connect, "SHOW TABLES FROM $dbname");
 
+# display tables for inspection
 $tblCnt = 0;
 while ($tbl = mysqli_fetch_array($result)) {
     $tblCnt++;
-    echo $tbl[0]."<br />\n";
+    echo $tbl[0] . "<br />\n";
 }
 
+# confirm the result
 if (!$tblCnt) {
     echo "There are no tables<br />\n";
 } else {
     echo "There are $tblCnt tables<br />\n";
 }
 
+# output all settings concerning the PHP installation
 phpinfo();
