b78099222012782e74105fa6611151d923a431cf
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2024-02-22 12:48:32 +0000

    Fix line endings and laravel mix react()

--- public/app/themes/justice/template-parts/panels/search.php
+++ public/app/themes/justice/template-parts/panels/search.php
@@ -5,7 +5,7 @@
     <div class="content">
         <form action="http://www.justice.gov.uk/courts/procedure-rules/civil/standard-directions/standard-directions-search" class="styled">
             <label for="searchbox">Search standard directions content</label> 
-            <input name="query" id="query" type="text" value="E.g. Witness Statements" accesskey="q" onfocus="this.value='';">
+            <input name="query" id="query" type="text" value="E.g. Witness Statements" accesskey="q" onfocus="this.value=''">
             <input class="go-btn" value="Search" type="submit"> 
             <input name="collection" value="MoJ-Standard-Directions" type="hidden"> 
             <input name="form" value="simple" type="hidden"> 
--- public/app/themes/justice/webpack.mix.js
+++ public/app/themes/justice/webpack.mix.js
@@ -5,6 +5,7 @@ mix.setPublicPath('./dist/')
 /*******************/
 mix.js('src/js/app.js', 'dist/app.min.js')
     .js('src/js/block-editor.js', 'dist/block-editor.min.js')
+    .react() // This allows us to compile JSX through import statements.
     .sass('src/sass/app.scss', 'dist/app.min.css')
     .copy('src/img/', 'dist/img/')
     .css('src/css/global.css', 'dist/css/')
@@ -12,7 +13,6 @@ mix.js('src/js/app.js', 'dist/app.min.js')
     .css('src/css/editor-style.css', 'dist/css/')
     .css('src/css/wp-admin-override.css', 'dist/css/')
     .options({ processCssUrls: false })
-    .react()
 
 if (mix.inProduction()) {
     mix.version()
