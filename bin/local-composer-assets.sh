d4bc5b69568220b0bc362f08290e51cc88025092
Damien Wilson <47327051+wilson1000-MoJ@users.noreply.github.com>
2024-02-29 13:56:48 +0000

    Local optimise (#64)
    * Composer install if needed
    
    * Remove DB information
    
    * Add AWS PHP-SDK compatibility check
    
    * Corrected comment
    
    * Remove locking and add comment
    
    * Add author
    
    * Remove trailing space
    
    * Rename local-kube to kind

--- bin/local-composer-assets.sh
+++ bin/local-composer-assets.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env ash
 
-# Nginx file sharing regex-parts
-regex_files='\(htm\|html\|js\|css\|png\|jpg\|jpeg\|gif\|ico\|svg\|webmanifest\)'
-regex_path='\(app\/themes\/justice\|app\/mu\-plugins\|app\/plugins\|wp\)'
+if [ ! -d "./vendor" ]; then
+  composer install
+fi
 
-composer install
+if [ ! -d "./vendor-assets" ]; then
+  # Nginx file sharing regex-parts
+  regex_files='\(htm\|html\|js\|css\|png\|jpg\|jpeg\|gif\|ico\|svg\|webmanifest\)'
+  regex_path='\(app\/themes\/justice\|app\/mu\-plugins\|app\/plugins\|wp\)'
 
-[ -d "./vendor-assets" ] || mkdir -p ./vendor-assets
-find public/ -name '*node_modules*' -prune -name '*justice/src*' -prune -name '*justice/webpack*' -prune -o -type f -regex "public\/${regex_path}.*\.${regex_files}" -exec cp --parent "{}" vendor-assets/  \;
+  echo "Generating vendor-assets directory..."
 
+  mkdir -p ./vendor-assets
+  find public/ -name '*node_modules*' -prune -name '*justice/src*' -prune -name '*justice/webpack*' -prune -o -type f -regex "public\/${regex_path}.*\.${regex_files}" -exec cp --parent "{}" vendor-assets/  \;
+
+  echo "Done."
+fi
