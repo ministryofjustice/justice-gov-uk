38f1fa77a9122a1bc0219cf804b23ebc7aaae52e
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2024-02-29 16:45:47 +0000

    phpcs formatting

--- public/app/themes/justice/page_errors.php
+++ public/app/themes/justice/page_errors.php
@@ -15,6 +15,6 @@ error_log('Source function: `error_log`. Source file: page_errors.php');
 
 trigger_error('Source function: `trigger_error`. Source file: page_errors.php', E_USER_WARNING);
 
-new WP_Error( 'exception', 'Source function: `new WP_Error`. Source file: page_errors.php' );
+new WP_Error('exception', 'Source function: `new WP_Error`. Source file: page_errors.php');
 
 throw new Exception("Source function: `throw new Exception`. Source file: page_errors.php", 900);
