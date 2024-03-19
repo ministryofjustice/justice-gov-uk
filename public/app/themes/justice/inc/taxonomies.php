bbbdf028c75b5310c80b9a5807b9ea8713db98dc
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2024-03-19 17:23:38 +0000

    Add taxonomies for search

--- public/app/themes/justice/inc/taxonomies.php
+++ public/app/themes/justice/inc/taxonomies.php
@@ -6,16 +6,135 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
+/**
+ * A class related to taxonomies.
+ *
+ * Includes adding taxonomies to the theme and getting taxonomies for the search filter.
+ */
+
 class Taxonomies
 {
 
-    public function addHooks()
+    public const TAXONOMY_DEFAULT = [
+        'object_type' => ['document', 'page'],
+        'args' => [
+            'show_admin_column' => true,
+            'rewrite' => false
+        ],
+    ];
+
+    public const TAXONOMY_SECTION = [
+        'taxonomy' => 'section',
+        'args' => [
+            'labels' => [
+                'name' => 'Sections',
+                'singular_name' => 'Section',
+                'plural_name' => 'Sections',
+                'add_new_item' => 'Add New Section',
+            ],
+        ],
+    ];
+
+    public const TAXONOMY_ORGANISATION = [
+        'taxonomy' => 'organisation',
+        'args' => [
+            'labels' => [
+                'name' => 'Organisations',
+                'singular_name' => 'Organisation',
+                'plural_name' => 'Organisations',
+                'add_new_item' => 'Add New Organisation',
+            ],
+        ],
+    ];
+
+    public const TAXONOMY_AUDIENCE = [
+        'taxonomy' => 'audience',
+        'args' => [
+            'labels' => [
+                'name' => 'Audiences',
+                'singular_name' => 'Audience',
+                'plural_name' => 'Audiences',
+                'add_new_item' => 'Add New Audience',
+            ],
+        ],
+    ];
+
+    public const TAXONOMY_TYPE = [
+        'taxonomy' => 'type',
+        'args' => [
+            'labels' => [
+                'name' => 'Types',
+                'singular_name' => 'Type',
+                'plural_name' => 'Types',
+                'add_new_item' => 'Add New Type',
+            ],
+        ],
+    ];
+
+    public const TAXONOMIES = [
+        self::TAXONOMY_SECTION,
+        self::TAXONOMY_ORGANISATION,
+        self::TAXONOMY_AUDIENCE,
+        self::TAXONOMY_TYPE,
+    ];
+
+    public function addHooks(): void
     {
         add_action('init', [$this, 'registerTaxonomies']);
     }
 
-    public function registerTaxonomies()
+    /**
+     * Register taxonomies.
+     *
+     * @return void
+     */
+
+    public function registerTaxonomies(): void
     {
+        // Enable tags for pages.
         register_taxonomy_for_object_type('post_tag', 'page');
+
+        // Loop through taxonomies and register them.
+        // Merge with default object type and args.
+        foreach (self::TAXONOMIES as $taxonomy) {
+            register_taxonomy(
+                $taxonomy['taxonomy'],
+                array_merge(self::TAXONOMY_DEFAULT['object_type'], isset($taxonomy['object_type']) ? $taxonomy['object_type'] : []),
+                array_merge(self::TAXONOMY_DEFAULT['args'], isset($taxonomy['args']) ? $taxonomy['args'] : []),
+            );
+        }
+    }
+
+    /**
+     * Returns an object with the taxonomies, labels and terms.
+     *
+     * @return array
+     */
+
+    public function getTaxonomiesForFilter() : array
+    {
+        // Get all taxonomies.
+        $taxonomies = get_object_taxonomies('page', 'objects');
+
+        // Filter out post_tag.
+        $taxonomies = array_filter($taxonomies, fn ($taxonomy) =>  $taxonomy->name !== 'post_tag');
+
+        // Map over the taxonomies and return an object with the name, label and terms.
+        return array_map(fn ($taxonomy) => (object) [
+            'name' => $taxonomy->name,
+            'label' => $taxonomy->labels->singular_name,
+            // Map over the terms here and return an object with the name, slug and selected.
+            'terms' => array_map(
+                fn ($term) => (object) [
+                    'name' => $term->name,
+                    'slug' => $term->slug,
+                    'selected' => isset($_GET[$taxonomy->name]) && $_GET[$taxonomy->name] === $term->slug,
+                ],
+                get_terms([
+                    'taxonomy'   =>  $taxonomy->name,
+                    'hide_empty' => false,
+                ])
+            ),
+        ], $taxonomies);
     }
 }
--- public/app/themes/justice/template-parts/panels/search-filters.php
+++ public/app/themes/justice/template-parts/panels/search-filters.php
@@ -1,86 +1,49 @@
+<?php
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+use MOJ\Justice\Taxonomies;
+
+$taxonomies = (new Taxonomies())->getTaxonomiesForFilter();
+
+if (empty($taxonomies)) {
+    return;
+}
+
+?>
+
 <div class="filter">
     <form>
         <span class="title">Filter results by:</span>
-        <label for="filter-section">Section</label>
-        <select id="filter-section" name="meta_S_phrase_sand" style="display: none;">
-            <option value="">All</option>
-            <option value="contacts">Contacts</option>
-            <option value="courts">Courts</option>
-            <option value="news">News</option>
-            <option value="offenders">Offenders</option>
-            <option value="publications">Publications</option>
-            <option value="tribunals">Tribunals</option>
-        </select>
-        <div id="nselect0" class="nselect" style="z-index:1000;" tabindex="0">
-            <div class="current">All</div>
-            <ul class="inner-list" style="display:none;">
-                <li class="option0"><span>All</span><input type="hidden" value=""></li>
-                <li class="option1"><span>Contacts</span><input type="hidden" value="contacts"></li>
-                <li class="option2"><span>Courts</span><input type="hidden" value="courts"></li>
-                <li class="option3"><span>News</span><input type="hidden" value="news"></li>
-                <li class="option4"><span>Offenders</span><input type="hidden" value="offenders"></li>
-                <li class="option5"><span>Publications</span><input type="hidden" value="publications"></li>
-                <li class="option6"><span>Tribunals</span><input type="hidden" value="tribunals"></li>
-            </ul>
-        </div>
-        <label for="filter-organisation">Organisation</label>
-        <select id="filter-organisation" name="meta_O_phrase_sand" style="display: none;">
-            <option value="">All</option>
-        </select>
-        <div id="nselect1" class="nselect" style="z-index:999;" tabindex="0">
-            <div class="current">All</div>
-            <ul class="inner-list" style="display:none;">
-                <li class="option0"><span>All</span><input type="hidden" value=""></li>
-            </ul>
-        </div>
-        <label for="filter-audience">Audience</label>
-        <select id="filter-audience" name="meta_U_phrase_sand" style="display: none;">
-            <option value="">All</option>
-            <option value="academic, researcher">Academic, Researcher</option>
-            <option value="legal profession">Legal Profession</option>
-            <option value="mediator, advice worker">Mediator, Advice Worker</option>
-            <option value="prison, probation officer">Prison, Probation Officer</option>
-            <option value="public, citizen">Public, Citizen</option>
-            <option value="representing myself at court">Representing Myself At Court</option>
-            <option value="youth worker">Youth Worker</option>
-        </select>
-        <div id="nselect2" class="nselect" style="z-index:998;" tabindex="0">
-            <div class="current">All</div>
-            <ul class="inner-list" style="display:none;">
-                <li class="option0"><span>All</span><input type="hidden" value=""></li>
-                <li class="option1"><span>Academic, Researcher</span><input type="hidden" value="academic, researcher"></li>
-                <li class="option2"><span>Legal Profession</span><input type="hidden" value="legal profession"></li>
-                <li class="option3"><span>Mediator, Advice Worker</span><input type="hidden" value="mediator, advice worker"></li>
-                <li class="option4"><span>Prison, Probation Officer</span><input type="hidden" value="prison, probation officer"></li>
-                <li class="option5"><span>Public, Citizen</span><input type="hidden" value="public, citizen"></li>
-                <li class="option6"><span>Representing Myself At Court</span><input type="hidden" value="representing myself at court"></li>
-                <li class="option7"><span>Youth Worker</span><input type="hidden" value="youth worker"></li>
-            </ul>
-        </div>
-        <label for="filter-type">Type</label>
-        <select id="filter-type" name="meta_e_phrase_sand" style="display: none;">
-            <option value="">All</option>
-            <option value="guidance">Guidance</option>
-            <option value="report">Report</option>
-        </select>
-        <div id="nselect3" class="nselect" style="z-index:997;" tabindex="0">
-            <div class="current">All</div>
-            <ul class="inner-list" style="display:none;">
-                <li class="option0"><span>All</span><input type="hidden" value=""></li>
-                <li class="option1"><span>Guidance</span><input type="hidden" value="guidance"></li>
-                <li class="option2"><span>Report</span><input type="hidden" value="report"></li>
-            </ul>
-        </div>
+
+        <?php foreach ($taxonomies as $taxonomy) : ?>
+            <div>
+
+                
+                <label for="filter-<?= $taxonomy->name; ?>"><?= $taxonomy->label; ?></label>
+                
+                <select id="filter-<?= $taxonomy->name; ?>" name="<?= $taxonomy->name; ?>">
+                    
+                <option value="">All</option>
+                <?php foreach ($taxonomy->terms as $term) : ?>
+                    <option value="<?= $term->slug; ?>" <?= $term->selected ? "selected" : '' ?> >
+                        <?= $term->name; ?>
+                    </option>
+                <?php endforeach; ?>
+                    
+                </select>
+            </div>
+                
+        <?php endforeach; ?>
+
         <label for="filter-web-only" id="filter-web-only-label">Web Pages Only</label>
-        <input type="checkbox" name="meta_f" id="filter-web-only" value="!pdf">
+        <input type="checkbox" name="web-only" id="filter-web-only" value="!pdf" />
+
         <div class="filter-btns">
-            <input class="go-btn" type="submit" value="Apply filter">
+            <input class="go-btn" type="submit" value="Apply filter" />
         </div>
 
-        <input type="hidden" name="form" value="simple">
-
-        <input type="hidden" name="profile" value="_default">
-        <input type="hidden" name="collection" value="moj-matrix-dev-web">
-        <input type="hidden" name="query" value="test">
     </form>
 </div>
