6d9efa6b28bf9e0547591f23cfa45805936b7658
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2024-03-19 11:10:15 +0000

    env.php readme update

--- config/env.php
+++ config/env.php
@@ -7,7 +7,8 @@ namespace MOJ\Justice;
  *
  * The vlucas/phpdotenv package is used to load environment variables from a .env file.
  * The defined variables are made available in the $_ENV and $_SERVER super-globals.
- * This function provides a convenient way to access these variables.
+ * This function provides a convenient way to access these variables, and variables
+ * that have been set directly to $_ENV.
  *
  * @param string $key
  * @return mixed
