91e7fd90b9966aea92f9615d0baea31e3c6d5210
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2024-04-29 09:49:59 +0100

    Use post-install script to fix typo

---
+++ bin/composer-post-install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env ash
+
+# Fix a typo in the wp-document-revisions plugin.
+
+# Define the search and replace strings.
+WPDR_FILE=public/app/plugins/wp-document-revisions/includes/class-wp-document-revisions.php
+WPDR_SEARCH=subdtr
+WPDR_REPLACE=subdir
+
+# If serach string is in file. Then replace it.
+if grep -q  $WPDR_SEARCH $WPDR_FILE ; then
+  echo "Fixing typo in wp-document-revisions..."
+  sed -i "s/$WPDR_SEARCH/$WPDR_REPLACE/g" $WPDR_FILE
+fi
