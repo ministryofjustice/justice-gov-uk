6914a7aaa526886c98ae5f4e4d0e3197c73879bb
Emily Hazlehurst <emily.hazlehurst@digital.justice.gov.uk>
2024-05-01 17:35:17 +0100

    Remove plugin branding and hide upsell menu item

--- public/app/themes/justice/inc/admin.php
+++ public/app/themes/justice/inc/admin.php
@@ -23,6 +23,8 @@ class Admin
     {
         add_action('admin_enqueue_scripts', array($this, 'enqueueStyles'));
         add_action('admin_menu', [$this, 'removeCustomizer'], 999);
+        add_action('admin_head', [$this, 'removePublishPressUpsellBranding']);
+
     }
 
     public static function enqueueStyles()
@@ -35,5 +37,30 @@ class Admin
         // We need this because the submenu's link (key from the array too) will always be generated with the current SERVER_URI in mind.
         $customizer_url = add_query_arg('return', urlencode(remove_query_arg(wp_removable_query_args(), wp_unslash($_SERVER['REQUEST_URI']))), 'customize.php');
         remove_submenu_page('themes.php', $customizer_url);
+    }
+
+    public static function removePublishPressUpsellBranding()
+    {
+        echo '<style>
+               .toplevel_page_revisionary-q {
+                 li:last-of-type {
+                   display: none;
+                 }
+               }
+               .pp-version-notice-bold-purple {
+                 display: none;
+               }
+               .pressshack-admin-wrapper {
+                 .inner-sidebar {
+                   display: none;
+                 }
+                 footer {
+                   display: none;
+                 }  
+               }
+               .ppseries-settings-body-content {
+                 width: 100% !important;
+               }
+             </style>';
     }
 }
