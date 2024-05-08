52e4b96f23e89ac3570c11aa8f840013d520a0eb
Emily Hazlehurst <emily.hazlehurst@digital.justice.gov.uk>
2024-05-08 14:33:48 +0100

    Run phpcs

--- public/app/themes/justice/inc/admin.php
+++ public/app/themes/justice/inc/admin.php
@@ -7,6 +7,8 @@
 
 namespace MOJ\Justice;
 
+use WP_REST_Request;
+
 if (!defined('ABSPATH')) {
     die();
 }
@@ -23,7 +25,7 @@ class Admin
     {
         add_action('admin_enqueue_scripts', array($this, 'enqueueStyles'));
         add_action('admin_menu', [$this, 'removeCustomizer'], 999);
-        add_filter('rest_page_query', [$this, 'increaseDropdownLimit'], 20);
+        add_filter('rest_page_query', [$this, 'increaseDropdownLimit'], 2, 20);
     }
 
     public static function enqueueStyles()
@@ -40,10 +42,18 @@ class Admin
     
     /**
      * Increase the dropdown limit on the block editor parent page field
+     *
+     * @param array $args An array of arguments for WP_Query
+     * @param WP_REST_Request $request The REST API request
+     *
+     * @return array $args The modified WP_Query array
      */
-    function increaseDropdownLimit( $args ) {
-        if ( is_user_logged_in() ) {
-          $args['posts_per_page'] = 2000;    
+    function increaseDropdownLimit($args, $request)
+    {
+        if (is_user_logged_in()) {
+            if ($request->get_query_params()['context'] === 'edit') {
+                $args['posts_per_page'] = 2000;
+            }
         }
         return $args;
     }
