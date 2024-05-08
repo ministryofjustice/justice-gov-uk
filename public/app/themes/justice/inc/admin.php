466e9fa20e72e869d34e1559a078871c83de0a33
Emily Hazlehurst <emily.hazlehurst@digital.justice.gov.uk>
2024-05-08 09:44:03 +0100

    CDPT-1562: Issue with dropdown - Increase the page limit on the dropdown editor

--- public/app/themes/justice/inc/admin.php
+++ public/app/themes/justice/inc/admin.php
@@ -23,6 +23,7 @@ class Admin
     {
         add_action('admin_enqueue_scripts', array($this, 'enqueueStyles'));
         add_action('admin_menu', [$this, 'removeCustomizer'], 999);
+        add_filter('rest_page_query', [$this, 'increaseDropdownLimit'], 20);
     }
 
     public static function enqueueStyles()
@@ -35,5 +36,15 @@ class Admin
         // We need this because the submenu's link (key from the array too) will always be generated with the current SERVER_URI in mind.
         $customizer_url = add_query_arg('return', urlencode(remove_query_arg(wp_removable_query_args(), wp_unslash($_SERVER['REQUEST_URI']))), 'customize.php');
         remove_submenu_page('themes.php', $customizer_url);
+    }
+    
+    /**
+     * Increase the dropdown limit on the block editor parent page field
+     */
+    function increaseDropdownLimit( $args ) {
+        if ( is_user_logged_in() ) {
+          $args['posts_per_page'] = 2000;    
+        }
+        return $args;
     }
 }
