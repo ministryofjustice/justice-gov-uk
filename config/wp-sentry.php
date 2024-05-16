e630129be36fa70bc470699b22aaa197dbf8b8a3
Emily Hazlehurst <emily.hazlehurst@digital.justice.gov.uk>
2024-05-16 11:39:37 +0100

    Remove commented code

--- config/wp-sentry.php
+++ config/wp-sentry.php
@@ -22,15 +22,3 @@ const WP_SENTRY_BROWSER_REPLAYS_SESSION_SAMPLE_RATE = 0.1; // replaysSessionSamp
 const WP_SENTRY_BROWSER_REPLAYS_ON_ERROR_SAMPLE_RATE = 1.0; // replaysOnErrorSampleRate
 
 require_once $root_dir . '/public/app/plugins/wp-sentry/wp-sentry.php';
-
-/**
- * Initialise Sentry
- */
-// if (env('SENTRY_DSN')) {
-//     Sentry\init([
-//         'dsn' => env('SENTRY_DSN'),
-//         'environment' => WP_ENV . (env('SENTRY_DEV_ID') ?? ''),
-//         'traces_sample_rate' => Config::get('SENTRY_TRACES_SAMPLE_RATE'),
-//         'profiles_sample_rate' => Config::get('SENTRY_PROFILE_SAMPLE_RATE')
-//     ]);
-// }
