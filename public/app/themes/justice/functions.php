5e60be21b00c6247438b5602752339fcad7e2085
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2024-05-29 00:07:33 +0100

    Override CCFW js files for fixes

--- public/app/themes/justice/functions.php
+++ public/app/themes/justice/functions.php
@@ -72,3 +72,25 @@ add_action('init', fn() => register_nav_menus([
     'header-menu' => __('Header Menu'),
     'footer-menu' => __('Footer Menu')
 ]));
+
+
+add_action('wp_enqueue_scripts', function () {
+    wp_deregister_script('ccfw-script-frontend');
+    wp_deregister_script('ccfw-script');
+
+    wp_enqueue_script(
+        'ccfw-script-frontend',
+        get_template_directory_uri() . '/dist/patch/js/ccfw-frontend.js',
+        ['jquery'],
+        1.0,
+        true
+    );
+
+    wp_enqueue_script(
+        'ccfw-script',
+        get_template_directory_uri() . '/dist/patch/js/ccfw-cookie-manage.js',
+        ['jquery'],
+        1.0,
+        true
+    );
+});
--- public/app/themes/justice/webpack.mix.js
+++ public/app/themes/justice/webpack.mix.js
@@ -3,10 +3,14 @@ require("@tinypixelco/laravel-mix-wp-blocks");
 
 mix.setPublicPath('./dist/')
 
-/*******************/
 mix
     .block("src/js/block-editor.js", "dist")
     .js('src/js/app.js', 'dist/app.min.js')
+    /** patch code for CCFW **/
+    .js('src/patch/js/ccfw-cookie-manage.js', 'dist/patch/js/ccfw-cookie-manage.js')
+    .js('src/patch/js/ccfw-frontend.js', 'dist/patch/js/ccfw-frontend.js')
+    .js('src/patch/js/ccfw-gtm.js', 'dist/patch/js/ccfw-gtm.js')
+    /** -------------- **/
     .sass('src/sass/app.scss', 'dist/css/app.min.css')
     .sass('src/sass/admin.scss', 'dist/css/admin.min.css')
     .sass('src/sass/editor.scss', 'dist/css/editor.min.css')
