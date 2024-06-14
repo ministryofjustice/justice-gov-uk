1fbab2dbe947339bb5caf98b33ea1deed99fbb72
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2024-06-14 16:03:22 +0100

    block editor move function to own file

--- public/app/themes/justice/src/js/block-editor.js
+++ public/app/themes/justice/src/js/block-editor.js
@@ -1,8 +1,8 @@
 // @ts-check
 /// <reference path="./block-editor.d.ts" />
 
-import { addFilter } from "@wordpress/hooks";
-
+import "./block-editor/filter-menu-more";
+import "./block-editor/remove-custom-class";
 import "../components/core-list/block-editor";
 import "../components/core-rich-text/block-editor";
 import "../components/inline-menu/block-editor";
@@ -10,36 +10,3 @@ import "../components/search/block-editor";
 import "../components/simple-definition-list-blocks/block-editor";
 import "../components/simple-guten-fields/index";
 import "../components/to-the-top/block-editor";
-
-/**
- * Removes support for custom class name on core blocks.
- *
- * @param {Object} settings
- * @param {string} name
- * @returns {Object}
- */
-
-const addListBlockClassName = (settings, name) => {
-  if (
-    !name.startsWith("core/") ||
-    settings.supports?.customClassName === false
-  ) {
-    return settings;
-  }
-
-  console.log(settings);
-
-  return {
-    ...settings,
-    supports: {
-      ...settings.supports,
-      customClassName: false,
-    },
-  };
-};
-
-addFilter(
-  "blocks.registerBlockType",
-  "moj/customise-blocks",
-  addListBlockClassName,
-);
