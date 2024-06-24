0de428b6f5b07fc6e0c03037d425023c5ff2faec
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2024-06-24 09:21:01 +0100

    check in experimental code

--- public/app/themes/justice/inc/documents/schedule.php
+++ public/app/themes/justice/inc/documents/schedule.php
@@ -175,4 +175,114 @@ trait DocumentSchedule
         <p style="padding-top: 10px;"><a href="<?php echo esc_url(add_query_arg('key', $key, get_post_comments_feed_link($post->ID))); ?>"><?php esc_html_e('RSS Feed', 'wp-document-revisions'); ?></a></p>
 <?php
     }
+
+    /**
+     * Save meta box content.
+     * 
+     * Non working - experimental
+     * 
+     *
+     * @param int $post_id Post ID
+     */
+    public function handleScheduleSave($doc_id)
+    {
+        // Save logic goes here. Don't forget to include nonce checks!
+
+        global $wpdb;
+        global $wpdr;
+        // Return early if the schedule field or global $wpdr is not set.
+        if (!isset($wpdr) || empty($_POST['schedule'])) {
+            return;
+        }
+
+        $content   = get_post_field('post_content', $doc_id);
+        $attach_id = $wpdr->extract_document_id($content);
+        $revisions    = $wpdr->admin->get_revisions($doc_id);
+
+        $schedule = sanitize_text_field($_POST['schedule']);
+        error_log('doc_id: ' . $doc_id);
+        error_log('schedule: ' . $schedule);
+        error_log('content: ' . $content);
+        error_log('attach_id: ' . $attach_id);
+
+        $mock_attach_id = 1234;
+        $mock_content = $wpdr->format_doc_id($mock_attach_id);
+        error_log('mock_content: ' . $mock_content);
+        error_log('revisions: ' . print_r($revisions, true));
+
+        // Get the last of the revisions array
+        $first_revision = $revisions[sizeof($revisions) - 1];
+        // error_log('first_revision: ' . print_r($first_revision, true));
+
+        $correct_post_content = $first_revision->post_content;
+
+        // if($revisions[1]) {
+
+        // 	// Revisions 0 the thing that we have just saved.
+        // 	// Set it's post_date & post_date_gmt to the schedule date.
+        // 	$sql = $wpdb->prepare(
+        // 		"UPDATE `{$wpdb->prefix}posts` SET `post_date` = %s, `post_date_gmt` = %s WHERE `id` = %d",
+        // 		$schedule,
+        // 		$schedule,
+        // 		$revisions[1]->ID
+        // 	);
+
+        // 	error_log('$revisions[1] : ' . print_r($revisions[1], true));
+        // }
+
+
+
+        // Update the $doc_id with this new content
+        $post_table = "{$wpdb->prefix}posts";
+        $sql        = $wpdb->prepare(
+            "UPDATE `$post_table` SET `post_content` = %s WHERE `id` = %d AND `post_parent` = 0 ",
+            $correct_post_content,
+            $doc_id
+        );
+        $res        = $wpdb->query($sql);
+        // phpcs:enable WordPress.DB.PreparedSQL.InterpolatedNotPrepared,WordPress.DB.PreparedSQL.NotPrepared,WordPress.DB.DirectDatabaseQuery
+        clean_post_cache($doc_id);
+
+        // Get all the revisions for the document
+
+        // Save the post_content field to something custome for a test
+
+
+
+    }
+
+    // Non working - experimental
+
+    public function onSaveRevision($post_has_changed, $last_revision, $post)
+    {
+        // only interested if post changed.
+        // if ( ! $post_has_changed ) {
+        // 	return $post_has_changed;
+        // }
+
+        // verify post type.
+        if (!$this->isDocument($post->ID)) {
+            return $post_has_changed;
+        }
+
+        error_log('post: ' . print_r($post, true));
+        error_log('last_revision: ' . print_r($last_revision, true));
+
+        // We know:
+        // previous attachment
+        // new (proposed attachment)
+        // Any existing scheduling metadata 
+
+        // We can
+        // Set some meta data on the post
+        // Update post_content directly by wpdb
+
+        // A scheduled task can manage posts with meta data, to update post_content with the correct attachment ID
+
+        // misuse of filter, but can use to determine whether the revisions can be merged.
+        // keep revision if title or content (document linked only) changed. Also if author changed.
+        if ($post->post_title !== $last_revision->post_title || $post->post_author !== $last_revision->post_author) {
+            return true;
+        }
+    }
 }
