22f6f3ad7a143a7020ee92d8a805ec5130787177
Emily Hazlehurst <emily.hazlehurst@digital.justice.gov.uk>
2024-07-04 13:04:27 +0100

    CDPT-1747: Nav main - Update font colours to meet contrast requirements - Reset ul list style - Add doc blocks to twig files - Add export - Style nav - Add Storybook example

---
+++ public/app/frontend/src/components/navigation-main/navigation-main.stories.js
@@ -0,0 +1,31 @@
+import navigationMain from './navigation-main.html.twig';
+import './index.js';
+
+export default {
+    title: 'Components/Navigation - Main',
+    parameters: {
+        layout: 'fullscreen',
+    },
+};
+
+const Template = (args) => {
+    return navigationMain(args);
+};
+
+export const Default = Template.bind({});
+Default.args = {
+    links: [
+        {
+            url: '#',
+            label: 'Courts',
+        },
+        {
+            url: '#',
+            label: 'Procedure rules'
+        },
+        {
+            url: '#',
+            label: 'Offenders'
+        }
+    ],
+};
