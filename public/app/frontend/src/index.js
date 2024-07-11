115952087b7003365defef7ecaf8cd38dd4d0156
Emily Hazlehurst <emily.hazlehurst@digital.justice.gov.uk>
2024-07-11 11:05:45 +0100

    Feature/cdpt 1747 component nav main (#221)
    * CDPT-1712: Initial setup
    - Config
    - Add docs
    - Install Timber and init in functions file
    - Set up webpack
    - Add build commands
    - Add linting commands
    - Define frontend structure
    - Add variables for styles
    - Add and configure storybook
    - Add icon files and logo
    - Add Button component
    - Add link component
    
    * - remove reference to missing layout files
    
    * CDPT-1714: Container and grid setup
    - Update base styles
    - Add grid docs
    - Add grid mixins
    - Add sidebar layout
    
    * - linting
    
    * - Add empty last line to files
    
    * nl
    
    * - Linting
    
    * - Remove unused import in grid docs
    - Rename with-sidebar to one-sidebar
    - Add two sidebar layout and example page
    
    * CDPT-1745: Link
    - Add visually hidden mixin from GDS
    - Update open in new link variant in accordance with GDS example
    - Use Heading group tag (hgroup) for headings with captions
    
    * CDPT-1719: Button
    - Update background colours to meet colour contrast requirements
    
    * CDPT-1747: Nav main
    - Update font colours to meet contrast requirements
    - Reset ul list style
    - Add doc blocks to twig files
    - Add export
    - Style nav
    - Add Storybook example
    
    * CDPT-1747: Nav main
    - Linting
    
    * - Add missing newTab var to docs

--- public/app/frontend/src/index.js
+++ public/app/frontend/src/index.js
@@ -2,7 +2,7 @@ import '../base.scss';
 
 // Components
 import './components/button';
-
+import './components/navigation-main';
 // Layouts
 import './layouts/one-sidebar';
 import './layouts/two-sidebars';
