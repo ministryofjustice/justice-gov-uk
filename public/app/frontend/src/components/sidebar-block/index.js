7639fe98d11b02986f59ee9e85da16d7c98bd9d7
Emily Hazlehurst <emily.hazlehurst@digital.justice.gov.uk>
2024-07-23 14:16:18 +0100

    CDPT-1746: List block - Fix focus issue on arrow button - Add sidebar block to index.js - Use sections and button on mobile for better accessibility

--- public/app/frontend/src/components/sidebar-block/index.js
+++ public/app/frontend/src/components/sidebar-block/index.js
@@ -7,14 +7,12 @@ export default function() {
     // If active, open child sublist by default - use atr to pass info on
     // Add arrow similar to in main nav to indicate?
 
-    // Layout columns. Add 8 margin top to all children except first. Remove this from individual components? Otherwise image won't work in second sidebar.
-
     if (!els) {
         return;
     }
 
     els.forEach((el) => {
-        const button = el.querySelector('.sidebar-block__show-more');
+        const button = el.querySelector('.sidebar-block__heading-button');
         const content = el.querySelector('.sidebar-block__content');
         button.addEventListener('click', () => {
             content.classList.toggle('sidebar-block__content--open')
