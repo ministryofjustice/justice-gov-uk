a5d90dcc0f7b7c24b7b177396f52128482f141e6
Emily Hazlehurst <emily.hazlehurst@digital.justice.gov.uk>
2024-08-02 16:24:57 +0100

    CDPT-1749: Navigation - sections - Add navigation component - Linting - Use grid columns for mobile layout

--- public/app/frontend/src/index.js
+++ public/app/frontend/src/index.js
@@ -12,6 +12,7 @@ import './components/header';
 import './components/image';
 import './components/image-with-text';
 import './components/file-download';
+import './components/navigation-sections';
 
 import sidebarBlock from '@components/sidebar-block';
 
