ce7c255d5375b26932150aefa2d13467fdcfe931
Emily Hazlehurst <emily.hazlehurst@digital.justice.gov.uk>
2024-08-05 16:21:15 +0100

    CDPT-1906: Amending and adding new link to panel - Clean up html

--- public/app/themes/justice/template-parts/panels/archived.php
+++ public/app/themes/justice/template-parts/panels/archived.php
@@ -10,14 +10,14 @@ defined('ABSPATH') || exit;
     <ul>
       <li>
         <a href="https://webarchive.nationalarchives.gov.uk/*/http://www.justice.gov.uk/index.htm">
-            Ministry of Justice archived websites (2007 to 2012)
+          Ministry of Justice archived websites (2007 to 2012)
         </a>
       </li>
-        <li>
-            <a href="https://webarchive.nationalarchives.gov.uk/ukgwa/timeline/https:/www.justice.gov.uk/courts/procedure-rules">
-                Ministry of Justice archived websites (2012 to present day)
-            </a>
-        </li>
+      <li>
+        <a href="https://webarchive.nationalarchives.gov.uk/ukgwa/timeline/https:/www.justice.gov.uk/courts/procedure-rules">
+          Ministry of Justice archived websites (2012 to present day)
+        </a>
+      </li>
     </ul>
   </div>
 </div>
