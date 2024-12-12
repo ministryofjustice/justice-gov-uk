f6d94ad72d23f463e29f25373207c4b5a3f0fdc4
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2024-12-12 16:28:30 +0000

    Update security.php

--- public/app/themes/justice/inc/security.php
+++ public/app/themes/justice/inc/security.php
@@ -105,6 +105,8 @@ class Security
 
     /**
      * Change the URL of the script or style tags.
+     * 
+     * @see https://developer.wordpress.org/reference/hooks/style_loader_tag/
      *
      * @param $tag string The HTML string of a link or script tag.
      * @return string The modified HTML string.
