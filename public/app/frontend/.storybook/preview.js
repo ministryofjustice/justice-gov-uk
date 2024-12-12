8d5cefb884464da60ca5c427ecfb51a299b7e7c2
Emily Hazlehurst <emily.hazlehurst@digital.justice.gov.uk>
2024-12-12 16:21:34 +0000

    CDPT- 1855: Update docs (#296)
    * CDPT- 1855: Update docs
    - Bring docs up to date
    - Add the beginnings of automated testing with test-runner.js
    - Add last reviewed block
    
    * nl
    
    * nl
    
    * - Update Setup instructions
    - Add required node version
    - Fix link

--- public/app/frontend/.storybook/preview.js
+++ public/app/frontend/.storybook/preview.js
@@ -1,4 +1,5 @@
 import '../src/index';
+import React from "react";
 
 // TODO: add agreed screensizes for Viewport button
 
@@ -10,13 +11,22 @@ const preview = {
         order: [
           'Documentation',
           'Development', ['Setup', 'Build a new component', 'Build a new layout', 'Connect a component to Wordpress'],
-          'Design', ['Typography', 'Colours', 'Grid', 'Icons'], 
-          'Quality assurance', ['Testing'], 
+          'Design', ['Typography', 'Colours', 'Grid', 'Spacing', 'Breakpoints', 'Icons'],
+          'Quality assurance', ['General', 'Compatibility', 'Accessibility', 'Performance'],
           'Components', 
           'Layouts', 
           'Example pages',
         ],
         locales: 'en-GB',
+      }
+    },
+    docs: {
+      components: {
+        // Override lists just for the Storybook theme (we reset them in base.scss)
+        ol: ({children, ...args}) => React.createElement('ol', {style: {'list-style-type': 'roman'}, ...args}, children),
+        ul: ({children, ...args}) => React.createElement('ul', {style: {'list-style-type': 'disc'}, ...args}, children),
+        // Override blockquotes just for Storybook (used for the 'review-block' Storybook component, see 'utils/documentation/review-block.js')
+        blockquote: ({children, ...args}) => React.createElement('blockquote', {style: {'margin-top': '5rem'}, ...args}, children)
       }
     }
   },
