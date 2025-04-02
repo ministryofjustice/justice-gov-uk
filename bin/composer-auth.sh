6dc1cb8ea991407f9dbcec6f166e0a4b416df46d
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2025-04-02 08:54:48 +0100

    Remove dependency on composer.dsd and update relevanssi & cookie-compliance-for-wordpress (#319)
    * Remove dependency on composer.dsd and update relevanssi & cookie-compliance-for-wordpress
    
    * Update Dockerfile
    
    * Update snyk-security.yml

--- bin/composer-auth.sh
+++ bin/composer-auth.sh
@@ -4,31 +4,31 @@
 # CONFIGURE COMPOSER AUTHENTICATION
 #
 # This script will create an `auth.json` file, which is used by composer for
-# HTTP basic auth access to the private composer repository composer.wp.dsd.io.
+# HTTP basic auth access to the private composer repository www.relevanssi.com.
 #
-# It requires the environment variables `COMPOSER_USER` and `COMPOSER_PASS` to
-# be set with authentication credentials.
+# It requires the environment variables `RELEVANSSI_API_KEY` to be set with 
+# authentication credentials.
 ##
 
 if [ -n "$COMPOSER_TOKEN" ]; then
   composer config --global github-oauth.github.com "$COMPOSER_TOKEN"
 fi
 
-if [ -n "$COMPOSER_USER" ] && [ -n "$COMPOSER_PASS" ]
+if [ -n "$RELEVANSSI_API_KEY" ]
 then
   rm -f auth.json
 	cat <<- EOF >> auth.json
 		{
 			"http-basic": {
-				"composer.wp.dsd.io": {
-					"username": "$COMPOSER_USER",
-					"password": "$COMPOSER_PASS"
+				"www.relevanssi.com": {
+					"username": "",
+					"password": "$RELEVANSSI_API_KEY"
 				}
 			}
 		}
 	EOF
 else
-	echo "FATAL: COMPOSER_USER and COMPOSER_PASS were not available."
+	echo "FATAL: RELEVANSSI_API_KEY was not available."
 fi
 
 ## check for auth.json
