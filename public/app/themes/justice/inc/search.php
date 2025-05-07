ab372a60c797abfd6a5a5c0811ac09bf9cfd85b3
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2025-05-07 12:22:27 +0100

    CDPT-2713 Relevanssi fix duplicate results

--- public/app/themes/justice/inc/search.php
+++ public/app/themes/justice/inc/search.php
@@ -48,6 +48,10 @@ class Search
         // Relevanssi - remove searches submenus for non-admins.
         add_filter('admin_menu', [$this, 'removeSearchesSubMenus'], 999);
 
+        // Relevanssi - fix duplicates due to revisions.
+        // https://www.relevanssi.com/knowledge-base/publishpress-revisions-duplicate-posts/
+        add_action( 'revision_applied', fn( $published, $revision ) => relevanssi_remove_doc( $revision->ID ), 10, 2 );
+
         // Redirect the user to the search page if the URI contains multiple pages.
         add_action('init', [$this, 'redirectMultiplePageInURI'], 1);
 
