9d8d9828c50af2ff18d5afcb69a77ca76ecfa856
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2025-08-08 14:29:28 +0100

    Update Content-Disposition for documents, when the permalink is updated.

--- public/app/themes/justice/inc/amazon-s3-and-cloudfront-tweaks-for-minio.php
+++ public/app/themes/justice/inc/amazon-s3-and-cloudfront-tweaks-for-minio.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * This file is cherry-picked functions from the wp-amazon-s3-and-cloudfront-tweaks plugin.
  * It is the config for using Minio Locally with WP Offload Media.
@@ -13,12 +14,13 @@
 
 namespace DeliciousBrains\WP_Offload_Media\Tweaks;
 
-use DeliciousBrains\WP_Offload_Media\Items\Item;
 use DeliciousBrains\WP_Offload_Media\Items\Media_Library_Item;
+use DeliciousBrains\WP_Offload_Media\Providers\Storage\AWS_Provider;
 
 class AmazonS3AndCloudFrontTweaks
 {
-
+    // This static property holds the instance of the amazon-s3-and-cloudfront plugin.
+    private static $as3cf = null;
 
     public function __construct()
     {
@@ -34,16 +36,38 @@ class AmazonS3AndCloudFrontTweaks
          * Custom S3 API Example: MinIO
          * @see https://min.io/
          */
-        add_filter('as3cf_aws_s3_client_args', array( $this, 'minioS3ClientArgs' ));
-        add_filter('as3cf_aws_s3_url_domain', array( $this, 'minioS3UrlDomain' ), 10, 5);
-        add_filter('as3cf_aws_s3_console_url', array( $this, 'minioS3ConsoleUrl' ));
-        add_filter('as3cf_aws_s3_console_url_prefix_param', array( $this, 'minioS3ConsoleUrlPrefixParam' ));
+        add_filter('as3cf_aws_s3_client_args', [$this, 'minioS3ClientArgs']);
+        add_filter('as3cf_aws_s3_url_domain', [$this, 'minioS3UrlDomain'], 10, 5);
+        add_filter('as3cf_aws_s3_console_url', [$this, 'minioS3ConsoleUrl']);
+        add_filter('as3cf_aws_s3_console_url_prefix_param', [$this, 'minioS3ConsoleUrlPrefixParam']);
 
         /*
          * URL Rewrite related filters.
          */
-        add_filter('as3cf_get_attachment_url', array( $this, 'getAttachmentUrl' ), 10, 4);
+        add_filter('as3cf_get_attachment_url', [$this, 'getAttachmentUrl'], 10, 4);
         add_filter('as3cf_use_ssl', '__return_false', 10, 1);
+
+        /*
+         * Set the as3cf as a property.
+         */
+        add_action('as3cf_ready', fn($as3cf) => self::$as3cf = $as3cf, 10, 1);
+
+        /*
+         * Update Content-Disposition for documents, when the permalink is updated.
+         * This is necessary to ensure that the document is downloaded with the correct filename.
+         */
+        add_action('document_permalink_updated', function ($document_id) {
+            // Ensure the as3cf instance is set before proceeding.
+            if (self::$as3cf) {
+                $this->updateContentDisposition($document_id);
+                return;
+            }
+
+            // We don't have self::$as3cf, so add an action to run updateContentDisposition when as3cf is ready.
+            add_action('as3cf_ready', function () use ($document_id) {
+                $this->updateContentDisposition($document_id);
+            }, 20);
+        });
     }
 
     // Define the minio hostnames for the backend and frontend contexts.
@@ -142,8 +166,8 @@ class AmazonS3AndCloudFrontTweaks
      * <<< MinIO Examples End
      */
 
-     
-     /*
+
+    /*
      * URL Rewrite related filters.
      */
 
@@ -165,6 +189,55 @@ class AmazonS3AndCloudFrontTweaks
     {
         // Replace the hostname for the browser.
         return str_replace(self::$minio_host_in_backend_context, self::$minio_host_in_frontend_context, $url);
+    }
+
+
+    /**
+     * This function updates the Content-Disposition header for a document attachment.
+     * 
+     * It uses the AWS S3 client to copy the object with the new Content-Disposition metadata.
+     * This is necessary to ensure that the document is downloaded with the correct filename.
+     * 
+     * @param int $document_id The ID of the document to update.
+     * @return void
+     */
+    public function updateContentDisposition($document_id): void
+    {
+        global $wpdr;
+
+        $attachments = $wpdr->get_attachments($document_id);
+        $latest_attachment = reset($attachments);
+
+        if (! $latest_attachment) {
+            return;
+        }
+
+        $as3cf_item = Media_Library_Item::get_by_source_id($latest_attachment->ID);
+
+        if (! $as3cf_item) {
+            return;
+        }
+
+        // Use reflection to get access to the private property, AWS_Provider->s3_client
+        // This is not supported by the plugin, :-0
+        // https://www.lambda-out-loud.com/posts/accessing-private-properties-php/#reflection-slow-but-clean
+        $provider = self::$as3cf->get_provider_client($as3cf_item->region());
+        $reflectionProperty = new \ReflectionProperty(AWS_Provider::class, 's3_client');
+        $reflectionProperty->setAccessible(true);
+        $s3_client = $reflectionProperty->getValue($provider);
+
+        $bucket = $as3cf_item->bucket();
+        $filename = basename(get_permalink($document_id));
+
+        // Use COPY on the $s3_client to update the Content Disposition metadata,
+        // since PUT cannot be used for partial updates.
+        $s3_client->copyObject([
+            'Bucket' => $bucket,
+            'CopySource' => "{$bucket}/{$as3cf_item->provider_key()}",
+            'Key' => $as3cf_item->provider_key(),
+            'MetadataDirective' => 'REPLACE',
+            'ContentDisposition' => 'attachment; filename="' . $filename . '"'
+        ]);
     }
 }
 
