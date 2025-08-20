ba46c0d2ff85147d1a718f6948c5045f4a799393
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2025-08-20 15:39:46 +0100

    Storybook component complete

--- public/app/frontend/src/components/navigation-secondary/navigation-secondary.stories.js
+++ public/app/frontend/src/components/navigation-secondary/navigation-secondary.stories.js
@@ -16,75 +16,66 @@ Default.args = {
     title: 'Justice UK',
     links: [
         {
+            id: 'courts',
             url: '#',
             label: 'Courts',
         },
         {
+            id: 'procedure-rules',
             url: '#',
             label: 'Procedure rules',
-            expanded: true,
             children: [
                 {
+                    id: 'procedure-rules-civil',
                     url: '#',
                     label: 'Civil',
                     children: [
                         {
+                            id: 'procedure-rules-civil-rules-and-practice-directions',
                             url: '#',
                             label: 'Rules & Practice Directions',
                         },
                         {
+                            id: 'procedure-rules-civil-standard-directions',
                             url: '#',
                             label: 'Standard directions',
                         },
                     ],
                 },
                 {
+                    id: 'procedure-rules-family',
                     url: '#',
                     label: 'Family',
-                    expanded: true,
                     children: [
                         {
+                            id: 'procedure-rules-family-foreword-and-summary',
                             url: '#',
                             label: 'Foreword and summary of the rules',
                         },
                         {
+                            id: 'procedure-rules-family-rules-and-practice-directions',
                             url: '#',
                             label: 'Rules and practice directions',
-                            expanded: true,
                             children: [
                                 {
+                                    id: 'procedure-rules-family-general-rules-and-definitions',
                                     url: '#',
                                     label: 'Part 1 – General rules and definitions',
                                 },
                                 {
+                                    id: 'procedure-rules-family-court-and-its-powers',
                                     url: '#',
                                     label: 'Part 2 – The court and its powers',
                                 },
                                 {
+                                    id: 'procedure-rules-family-case-management',
                                     url: '#',
                                     label: 'Part 3 – Case management',
-                                    expanded: true,
                                     children: [
                                         {
+                                            id: 'procedure-rules-family-case-management-powers-of-the-court',
                                             url: '#',
                                             label: 'Part 3A – Case management powers of the court',
-                                        },
-                                        {
-                                            url: '#',
-                                            label: 'Part 3B – Case management powers of the court in family proceedings',
-                                            expanded: true,
-                                            children: [
-                                                {
-                                                    url: '#',
-                                                    label: 'Part 3B.1 – Case management powers of the court in family proceedings',
-                                                    active: true,
-                                                },
-                                                {
-                                                    url: '#',
-                                                    label: 'Part 3B.2 – Case management powers of the court in family proceedings (children)',
-                                                    expanded: true,
-                                                },
-                                            ]
                                         },
                                     ],
                                 },
@@ -93,16 +84,19 @@ Default.args = {
                     ],
                 },
                 {
+                    id: 'other-procedure-rules',
                     url: '#',
                     label: "Other procedure rules for magistrates' courts and the Crown Court",
                 },
                 {
+                    id: 'procedure-rules-criminal',
                     url: '#',
                     label: 'Criminal',
                 },
             ],
         },
         {
+            id: 'offenders',
             url: '#',
             label: 'Offenders',
         },
@@ -115,11 +109,7 @@ ActiveTopLevelLink.args = {
     title: Default.args.title,
     links: Default.args.links.map((link) => {
         if (link.label === 'Courts') {
-            return {
-                url: link.url,
-                label: link.label,
-                active: true,
-            };
+            return {...link, active: true};
         }
         return link;
     }),
@@ -131,13 +121,27 @@ ActiveTopLevelButton.args = {
     title: Default.args.title,
     links: Default.args.links.map((link) => {
         if (link.label === 'Procedure rules') {
-            return {
-                url: link.url,
-                label: link.label,
-                active: true,
-                children: link.children,
-            };
+           return {...link, active: true, expanded: true };
         }
         return link;
     }),
+};
+
+export const ActiveNestedButton = Template.bind({});
+ActiveNestedButton.args = {
+    articleId: Default.args.articleId,
+    title: Default.args.title,
+    links: () => {
+        // Clone Default.args.links so that the original is not modified.
+        const linksWithNestedActive = JSON.parse(JSON.stringify(Default.args.links));
+
+        // Set the expanded and active states for the nested links.
+        linksWithNestedActive[1].expanded = true;
+        linksWithNestedActive[1].children[1].expanded = true;
+        linksWithNestedActive[1].children[1].children[1].expanded = true;
+        linksWithNestedActive[1].children[1].children[1].children[2].expanded = true;
+        linksWithNestedActive[1].children[1].children[1].children[2].children[0].active = true;
+
+        return linksWithNestedActive;
+    },
 };
