b24ad771a591a7890a9eb53c896e0fbaf40ebb19
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2025-09-02 16:17:29 +0100

    Move logic from the page template file to a controller file.

---
+++ public/app/themes/justice/inc/page/page.php
@@ -0,0 +1,127 @@
+<?php
+
+/**
+ * This class is responsible for the logic related to the template page.php
+ *
+ * The logic is located in this controller file, leaving the template file (page.php) 
+ * clean and focused on passing prepared values to Timber.
+ */
+
+namespace MOJ\Justice;
+
+defined('ABSPATH') || exit;
+
+require_once 'constants.php';
+
+use MOJ\Justice\NavigationSecondary;
+use MOJ\Justice\PostMeta;
+use MOJ\Justice\PageConstants;
+
+class PageController
+{
+
+    // Variable to hold the PostMeta instance.
+    public $post_meta = null;
+
+    /**
+     * Constructor.
+     * Initializes the PostMeta instance with the current post ID.
+     * This allows the controller to access post metadata and side panels.
+     */
+    public function __construct()
+    {
+        $this->post_meta = new PostMeta(get_the_ID());
+    }
+
+    /**
+     * Get the template to render.
+     * This checks if there are any left side panels and returns the appropriate template.
+     * If there are no left side panels, it uses a single sidebar template.
+     * Otherwise, it uses a two sidebar template.
+     *
+     * @return string The name of the template to render.
+     */
+    public function getTemplate(): string
+    {
+        return $this->post_meta->sideHasPanels('left') ? 'templates/basic--two-sidebars.html.twig' : 'templates/basic--one-sidebar.html.twig';
+    }
+
+
+    /**
+     * Get the left side panels.
+     * 
+     * This checks if there are any left side panels (currently only the menu).
+     * If there are no left side panels or the menu panel is not enabled, it returns an empty array.
+     * Otherwise, it returns an array with the menu panel and its links.
+     *
+     * @return array The left side panels, or an empty array if none are enabled.
+     */
+    public function getLeftSidePanels(): array
+    {
+        if (!$this->post_meta->sideHasPanels('left') || !$this->post_meta->hasPanel('menu')) {
+            return [];
+        }
+
+        return [
+            'menu' => [
+                ...PageConstants::PANELS_LEFT['menu'],
+                'links' => (new NavigationSecondary)->getCurrentPageNavigation()
+            ]
+        ];
+    }
+
+
+    /**
+     * Get the right side panels.
+     *
+     * This checks if there are any right side panels enabled.
+     * If there are no right side panels, it returns an empty array.
+     * Otherwise, it constructs an array of right side panels based on the constants defined in PageConstants.
+     * It populates the links for the 'related' and 'other_websites' panels with the corresponding metadata from the post.
+     *
+     * @return array The right side panels, or an empty array if none are enabled.
+     */
+    public function getRightSidePanels(): array
+    {
+        if (!$this->post_meta->sideHasPanels('right')) {
+            return [];
+        }
+
+        $side_panels = [];
+
+        foreach (PageConstants::PANELS_RIGHT as $panel => $variant) {
+            if ($this->post_meta->hasPanel($panel)) {
+
+                // If the panel is related or other_websites then populate the links array.
+                if ('related' === $panel) {
+                    // TODO - format links with opens in new bab if external. And download link if file.
+                    $variant['links'] = $this->post_meta->getMeta('_panel_related_entries');
+                }
+
+                if ('other_websites' === $panel) {
+                    // TODO - format links with opens in new bab if external. And download link if file.
+                    $variant['links'] = $this->post_meta->getMeta('_panel_other_websites_entries');
+                }
+
+                $side_panels[$panel] = $variant;
+            }
+        }
+
+        return $side_panels;
+    }
+
+
+    /**
+     * Get the updated date.
+     * 
+     * This checks if the '_show_updated_at' meta field is set for the post.
+     * If it is set, it returns the modified date formatted as specified.
+     * If not, it returns null.
+     * 
+     * @return string|null The formatted updated date or null if not set.
+     */
+    public function getUpdatedAt(): string|null
+    {
+        return $this->post_meta->getMeta('_show_updated_at') ? $this->post_meta->getModifiedAt() : null;
+    }
+}
--- public/app/themes/justice/page.php
+++ public/app/themes/justice/page.php
@@ -1,143 +1,30 @@
 <?php
+
+defined('ABSPATH') || exit;
+
 /**
- * The template for displaying a basic page
- *
+ * The template for displaying a generic page
  */
 
-use MOJ\Justice\PostMeta;
-use MOJ\Justice\NavigationSecondary;
-use MOJ\Justice\Content;
+use Timber\Timber;
+use MOJ\Justice\PageController;
+
+$page_controller = new PageController();
 
 get_header();
 
-$postMeta = new PostMeta(get_the_ID());
-
-$contentHelper = new Content();
-
-$breadcrumbs = array_map(function ($link) {
-    return [
-        'label' => $link['title'],
-        'url' => $link['url'],
-    ];
-}, (new MOJ\Justice\Breadcrumbs)->getTheBreadcrumbs());
-
-$relatedPages = $postMeta->getMeta('_panel_related_entries');
-$otherWebsites = array_map(function ($link) use ($contentHelper) {
-    return [
-        'label' => $link['label'],
-        'url' => $link['url'],
-        'newTab' => $contentHelper->isExternal($link['url'])
-    ];
-}, $postMeta->getMeta('_panel_other_websites_entries'));
-
-$sidePanelsRight = [];
-$sidePanelsLeft = [];
-
-$allowedRightPanels = [
-    'brand' => [
-        'variant' => 'brand',
-    ],
-    'search' => [
-        'variant' => 'form',
-        'title' => 'Search this collection',
-        'description' => 'Search standard directions',
-        'form' => [
-            'id' => 'search-bar-sidebar',
-            'action' => '/search',
-            'input' => [
-                'labelHidden' => true,
-                'label' => 'Search the standard directions content',
-                'id' => 'search-bar-sidebar-input',
-                'name' => 's',
-            ],
-            'button' => [
-                'text' => 'Search',
-            ],
-            'hiddenInputs' => [
-                [
-                    'name' => 'parent',
-                    'value' => '153',
-                ],
-            ],
-        ],
-    ],
-    'email_alerts' => [
-        'variant' => 'form',
-        'title' => 'Get email alerts',
-        'description' => 'Enter your email address below to subscribe to email alerts',
-        'form' => [
-            'id' => 'email-alerts-sidebar',
-            'action' => 'https://public.govdelivery.com/accounts/UKMOJ/subscribers/qualify',
-            'input' => [
-                'name' => 'email',
-                'labelHidden' => true,
-                'label' => 'Enter email address:',
-                'id' => 'email-alerts-input',
-            ],
-            'button' => [
-                'text' => 'Subscribe',
-            ],
-        ],
-    ],
-    ... $relatedPages ? ['related' => [
-        'variant' => 'list',
-        'title' => 'Related pages',
-        'links' => $relatedPages,
-    ]] : [],
-    ... $otherWebsites ? ['other_websites' => [
-        'variant' => 'list',
-        'title' => 'Other websites',
-        'links' => $otherWebsites,
-    ]] : [],
-    'archived' => [
-        'variant' => 'archive',
-        'title' => 'Archived pages',
-        'links' => [
-            [
-                'url' => 'https://webarchive.nationalarchives.gov.uk/*/http://www.justice.gov.uk/index.htm',
-                'label' => 'Ministry of Justice archived websites (2007 to 2012)',
-                'newTab' => true,
-            ],
-            [
-                'url' => 'https://webarchive.nationalarchives.gov.uk/ukgwa/timeline/https:/www.justice.gov.uk/courts/procedure-rules',
-                'label' => 'Ministry of Justice archived websites (2012 to present day)',
-                'newTab' => true,
-            ],
-        ],
-    ],
-];
-
-// Check if there are any right side panels if there are get the content for the variant
-if ($postMeta->sideHasPanels('right')) {
-    foreach ($allowedRightPanels as $panel => $variant) {
-        if ($postMeta->hasPanel($panel)) {
-            $sidePanelsRight[$panel] = $variant;
-        }
-    }
-}
-
-// Check if there are any left side panels (currently only the menu)
-if ($postMeta->sideHasPanels('left')) {
-    if ($postMeta->hasPanel('menu')) {
-        $sidePanelsLeft['menu'] = [
-            'title' => 'Justice UK',
-            'id' => '#main-page-content',
-            'links' => (new NavigationSecondary)->getCurrentPageNavigation()
-        ];
-    }
-}
-
-// If there are no side panels on the left use the single sidebar template
-$templates = !$sidePanelsLeft ? ['templates/basic--one-sidebar.html.twig'] : ['templates/basic--two-sidebars.html.twig'];
 $context = Timber::context([
     'title' => get_the_title(),
     'homeUrl' => home_url(),
     'permalink' => get_the_permalink(),
-    'breadcrumbs' => $breadcrumbs,
-    'updatedDate' => $postMeta->getMeta('_show_updated_at') ? $postMeta->getModifiedAt() : null,
-    'sidePanelsRight' => $sidePanelsRight,
-    'sidePanelsLeft' => $sidePanelsLeft,
+    'breadcrumbs' => (new MOJ\Justice\Breadcrumbs)->getTheBreadcrumbs(),
+    'updatedDate' => $page_controller->getUpdatedAt(),
+    'sidePanelsRight' => $page_controller->getRightSidePanels(),
+    'sidePanelsLeft' => $page_controller->getLeftSidePanels(),
 ]);
+
+$templates = [$page_controller->getTemplate()];
+
 Timber::render($templates, $context);
 
 get_footer();
