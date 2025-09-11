d39a2336a34178a96e71316c5051c99a99491048
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2025-09-11 15:58:11 +0100

    Optimise `ContentQualityIssueThead`

--- public/app/themes/justice/inc/content-quality/issues/thead.php
+++ public/app/themes/justice/inc/content-quality/issues/thead.php
@@ -17,6 +17,23 @@ final class ContentQualityIssueThead extends ContentQualityIssue
     const ISSUE_LABEL = 'Table without header section';
 
 
+    public function addHooks(): void
+    {
+        // Call the parent class addHooks method.
+        parent::addHooks();
+
+        // Create a scheduled task to run `processPages` every minute.
+        add_action('init', function () {
+            if (!wp_next_scheduled('moj_content_quality_thead_cron')) {
+                $jitter_between_runs = rand(0, 59);
+                wp_schedule_event(time() + $jitter_between_runs, 'one_minute', 'moj_content_quality_thead_cron');
+            }
+        });
+
+        // Hook the cron job to the processPages method.
+        add_action('moj_content_quality_thead_cron', [$this, 'processPages']);
+    }
+
     /**
      * Get the pages with thead issues.
      *
@@ -38,28 +55,82 @@ final class ContentQualityIssueThead extends ContentQualityIssue
         $query = "
             SELECT 
                 ID,
-                post_title,
-                post_status,
-                CHAR_LENGTH(post_content) - CHAR_LENGTH( REPLACE ( post_content, '<table', SPACE(LENGTH('<table')-1) ) ) AS table_count,
-                CHAR_LENGTH(post_content) - CHAR_LENGTH( REPLACE ( post_content, '<thead', SPACE(LENGTH('<thead')-1) ) ) AS thead_count
-            FROM {$wpdb->posts}
-            WHERE 
+                COALESCE(options.option_value, 'queued') AS issue_count
+            FROM {$wpdb->posts} AS p
+            -- To save us from running get_transient in a php loop, 
+            -- we can join the options table to get the transient value here
+            LEFT JOIN {$wpdb->options} AS options 
+            ON options.option_name = CONCAT('_transient_moj:content-quality:issue:thead:', p.ID)
+            LEFT JOIN {$wpdb->postmeta} AS postmeta
+            ON postmeta.post_id = ID AND postmeta.meta_key = '_content_quality_exclude'
+            WHERE
+                -- options value should be null or not zero
+                ( options.option_value IS NULL OR options.option_value != '0' ) AND
                 post_type = 'page' AND
-                post_content LIKE '%<table%' AND
-                CHAR_LENGTH(post_content) - CHAR_LENGTH( REPLACE ( post_content, '<table', SPACE(LENGTH('<table')-1)) ) > 
-                CHAR_LENGTH(post_content) - CHAR_LENGTH( REPLACE ( post_content, '<thead', SPACE(LENGTH('<thead')-1)) )
-            ORDER BY table_count DESC
+                post_status IN ('publish', 'private', 'draft') AND
+                (postmeta.meta_value IS NULL OR postmeta.meta_value = '0')
         ";
 
         foreach ($wpdb->get_results($query) as $page) {
             // Add the page to the pages with issue.
-            $pages_with_issue[$page->ID] = (object)[
-                'ID' => $page->ID,
-                'table_without_thead' => $page->table_count - $page->thead_count,
-            ];
+            $pages_with_issue[$page->ID] = $page->issue_count;
         }
 
         return $pages_with_issue;
+    }
+
+    /**
+     * Get the pages with thead issues.
+     *
+     * This function runs an SQL query to find pages with tables that do not have a <thead> element.
+     *
+     * @return array An array of pages with thead issues.
+     */
+    public function processPages(): void
+    {
+        $transient_updates = [];
+
+        // Run an SQL query to find pages with tables that do not have a <thead> element.
+        global $wpdb;
+
+        // The following query uses a hack to count occurrences of <table> and <thead> in the post_content.
+        // To count the occurrences of a substring in a string, we use a statement following the pattern:
+        // CHAR_LENGTH(<haystack>) - CHAR_LENGTH(REPLACE(<haystack>, <needle>, SPACE(LENGTH(<needle>)-1)))
+
+        $query = "
+            SELECT 
+                ID,
+                post_title,
+                post_status,
+                ( CHAR_LENGTH(post_content) - CHAR_LENGTH( REPLACE ( post_content, '<table', SPACE(LENGTH('<table')-1) ) ) ) -
+                ( CHAR_LENGTH(post_content) - CHAR_LENGTH( REPLACE ( post_content, '<thead', SPACE(LENGTH('<thead')-1) ) ) ) AS tables_without_thead
+            FROM {$wpdb->posts} AS p
+            LEFT JOIN {$wpdb->options} AS options 
+            ON options.option_name = CONCAT('_transient_moj:content-quality:issue:empty-heading:', p.ID)
+            LEFT JOIN {$wpdb->postmeta} AS postmeta
+            ON postmeta.post_id = ID AND postmeta.meta_key = '_content_quality_exclude'
+            WHERE 
+                options.option_value IS NULL AND
+                post_type = 'page' AND
+                post_status IN ('publish', 'private', 'draft') AND
+                (postmeta.meta_value IS NULL OR postmeta.meta_value = '0') AND
+                post_content LIKE '%<table%' AND
+                CHAR_LENGTH(post_content) - CHAR_LENGTH( REPLACE ( post_content, '<table', SPACE(LENGTH('<table')-1)) ) > 
+                CHAR_LENGTH(post_content) - CHAR_LENGTH( REPLACE ( post_content, '<thead', SPACE(LENGTH('<thead')-1)) )
+        ";
+
+        foreach ($wpdb->get_results($query) as $page) :
+            // Add the value to the transient updates array, this will be used in a bulk update later.
+            $transient_updates["$this->transient_key:{$page->ID}"] = $page->tables_without_thead;
+        endforeach;
+
+        if (sizeof($transient_updates)) {
+            $expiry = time() + $this->transient_duration;
+            $this->bulkSetTransientInDatabase($transient_updates, $expiry);
+
+            // Individual page transients have been updated, so clear the cache for this issue as a whole.
+            delete_transient($this->transient_key);
+        }
     }
 
 
@@ -81,7 +152,13 @@ final class ContentQualityIssueThead extends ContentQualityIssue
             return $issues;
         }
 
-        $count = $this->pages_with_issue[$post_id]->table_without_thead;
+        // If the issue is 'queued', then append the appropriate message.
+        if ('queued' === $this->pages_with_issue[$post_id]) {
+            $issues[] = __('The page is queued for heading without content issues.', 'justice');
+            return $issues;
+        }
+
+        $count = $this->pages_with_issue[$post_id];
 
         $issues[] =  sprintf(_n('There is %d table without a header section', 'There are %d tables without a header section', $count, 'justice'), $count);
 
