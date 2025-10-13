4ed1a94af78f4fc50863320c9948e205e66a71da
earthlingdavey <15802017+earthlingdavey@users.noreply.github.com>
2025-10-13 12:36:05 +0100

    CDPTKAN-109 Add security headers  (#396)
    * CSP WIP
    
    * Remove gravatar
    
    * Remove tracking from onclick attributes
    
    * Update header.php
    
    * Remove inline scripts
    
    * Add CSP
    
    * Add external image check to the Content Quality framework.
    
    * Run phpcbf
    
    * Update the prod nginx config
    
    * Update server.conf
    
    * Add security headers to a conf file
    
    * Update development.php
    
    * Update core.php
    
    * Update header.php
    
    * Removve banner haders
    
    * Hilight external images in the block editor
    
    * Update docker-compose.yml

--- public/app/themes/justice/inc/core.php
+++ public/app/themes/justice/inc/core.php
@@ -33,6 +33,31 @@ class Core
         add_filter('pre_http_request', [$this, 'handleLoopbackRequests'], 10, 3);
         // Remove Available Tools from the admin menu.
         add_action('admin_menu', [$this, 'removeSubmenus']);
+        // Remove the Gravatar service, and always show the default avatar.
+        add_filter('pre_get_avatar', [__CLASS__, 'replaceGravatar'], 10, 3);
+
+        // Remove inline css blocks...
+
+        // Dequeue the core block styles in the footer.
+        add_action('wp_footer', fn () => wp_dequeue_style('core-block-supports'));
+        // Remove the global styles that are added by WordPress.
+        remove_action('wp_enqueue_scripts', 'wp_enqueue_global_styles');
+        // Remove auto-sizes style that's been added by WordPress.
+        add_filter('wp_img_tag_add_auto_sizes', '__return_false');
+        // Remove the classic theme styles.
+        add_action('wp_enqueue_scripts', fn() => wp_dequeue_style('classic-theme-styles'), 20);
+
+        // Remove inline script blocks...
+
+        // Remove the customizer script.
+        add_action('add_admin_bar_menus', function () {
+            remove_action('admin_bar_menu', 'wp_admin_bar_customize_menu', 40);
+        }, 100);
+
+        // Remove the emoji detection script.
+        add_action('admin_print_scripts', function () {
+            remove_action('admin_print_scripts', 'print_emoji_detection_script');
+        }, 1);
     }
 
     /**
@@ -112,5 +137,24 @@ class Core
     public function removeSubmenus(): void
     {
         remove_submenu_page('tools.php', 'tools.php');
+    }
+
+
+    /**
+     * Replace Gravatar with a default avatar.
+     *
+     * This way we are not exposing the website's users to the Gravatar service.
+     *
+     * @param string|false $avatar
+     * @param int|string|WP_User $id_or_email
+     * @param array $args
+     * @return string
+     */
+    public static function replaceGravatar($_avatar, $_id_or_email, $args) : string
+    {
+        $width = isset($args['width']) && is_int($args['width']) ? $args['width'] : 26;
+        $height = isset($args['height']) && is_int($args['height']) ? $args['height'] : 26;
+
+        return sprintf('<img class="avatar avatar-26 photo" width="%d" height="%d" src="/app/themes/justice/dist/img/avatar.jpg" />', $width, $height);
     }
 }
