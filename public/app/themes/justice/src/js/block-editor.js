4e8a5addf7c18fc6289c786d8e6c197e3b3333e6
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2026-01-22 17:43:55 +0000

    Revert "Remove stretchy block variations."
    This reverts commit 0cd7f092f6172e7f74bb7d7f80cb5ea2e21ad60d.

--- public/app/themes/justice/src/js/block-editor.js
+++ public/app/themes/justice/src/js/block-editor.js
@@ -2,7 +2,6 @@
 /// <reference path="./block-editor.d.ts" />
 
 import "./block-editor/filter-menu-more";
-import "./block-editor/remove-block-variations";
 import "./block-editor/remove-custom-class";
 import "../components/core-image/block-editor";
 import "../components/core-list/block-editor";
