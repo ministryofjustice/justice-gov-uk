513475fb7beeab9cb81760325c127c2c9e0862ef
earthlingdavey <15802017+earthlingdavey@users.noreply.github.com>
2026-07-20 15:05:07 +0100

    Block batch endpoint (#550)
    * Block batch endpoint
    
    * Update functions.php

--- public/app/themes/justice/functions.php
+++ public/app/themes/justice/functions.php
@@ -124,3 +124,26 @@ add_action('init', function () {
             break;
     }
 });
+
+/**
+ * Reject anonymous requests to the core REST batch endpoint.
+ *
+ * @param mixed           $result  Pre-calculated dispatch result.
+ * @param WP_REST_Server  $server  REST server instance.
+ * @param WP_REST_Request $request Current REST request.
+ * @return mixed|WP_Error
+ */
+function wporg_require_authentication_for_rest_batch($result, $server, $request)
+{
+    if ('/batch/v1' !== strtolower(untrailingslashit($request->get_route())) || is_user_logged_in()) {
+        return $result;
+    }
+
+    return new WP_Error(
+        'rest_batch_authentication_required',
+        'Authentication is required to use the batch API.',
+        array( 'status' => 401 )
+    );
+}
+
+add_filter('rest_pre_dispatch', 'wporg_require_authentication_for_rest_batch', -1000, 3);
